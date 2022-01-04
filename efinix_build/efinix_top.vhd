3fc43acc2c4c0bbf97207a9e376cdd5a6500b36c
jari <jaarhonka@yahoo.com>
2022-01-04 19:56:22 +0200

    test led blinker with fpga

--- efinix_build/efinix_top.vhd
+++ efinix_build/efinix_top.vhd
@@ -15,9 +15,41 @@ end entity top;
 
 architecture rtl of top is
 
-    signal counter : counter_object_record := init_counter; 
-    signal counter2 : counter_object_record := init_counter; 
-    signal led_state : std_logic := '0'; 
+    type led_blinker_record is record
+        fast_counter : counter_object_record;
+        slow_counter : counter_object_record;
+        led_state    : std_logic;
+    end record;
+    
+    constant init_led_blinker : led_blinker_record := (init_counter, init_counter, '0');
+
+    procedure create_led_blinker
+    (
+        signal led_blinker_object : inout led_blinker_record;
+        signal led_out : out std_logic;
+        slow_counter_value : in natural range 1000 to 15e3
+    ) is
+        alias counter is led_blinker_object.fast_counter;
+        alias counter2 is led_blinker_object.slow_counter;
+        alias led_state    is led_blinker_object.led_state   ;
+
+    begin
+            led_out <= led_state;
+            create_timebase_from(counter, 10e3);
+            if counter_is_ready(counter) then
+                create_timebase_from(counter2, slow_counter_value);
+            end if;
+
+            if counter_is_ready(counter) and counter_is_ready(counter2) then
+                led_state <= not led_state;
+            end if;
+        
+    end create_led_blinker;
+
+    signal led_blinker0 : led_blinker_record := init_led_blinker;
+    signal led_blinker1 : led_blinker_record := init_led_blinker;
+    signal led_blinker2 : led_blinker_record := init_led_blinker;
+    signal led_blinker3 : led_blinker_record := init_led_blinker;
 
 begin
 
@@ -26,17 +58,11 @@ begin
         
     begin
         if rising_edge(clk_120MHz) then
-            leds <= (led_state, led_state, led_state, led_state);
-            create_timebase_from(counter, 10e3);
-            if counter_is_ready(counter) then
-                create_timebase_from(counter2, 10e3);
-            end if;
-
-            if counter_is_ready(counter) and counter_is_ready(counter2) then
-                led_state <= not led_state;
-            end if;
+            create_led_blinker(led_blinker0, leds(0), 2000);
+            create_led_blinker(led_blinker1, leds(1), 4000);
+            create_led_blinker(led_blinker2, leds(2), 6000);
+            create_led_blinker(led_blinker3, leds(3), 8000);
         end if; --rising_edge
     end process led_blinker;	
-
 
 end rtl;
