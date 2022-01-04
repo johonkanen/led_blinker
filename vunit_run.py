750cdba89cee405c6bb5ca23660b04a426fd3bf0
jari <jaarhonka@yahoo.com>
2022-01-04 17:44:53 +0200

    add testbench for blinker and vunit run script

---
+++ vunit_run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from vunit import VUnit
+
+# ROOT
+ROOT = Path(__file__).resolve().parent
+VU = VUnit.from_argv()
+VU = VUnit.from_argv(vhdl_standard="93")
+
+lib = VU.add_library("lib");
+# lib.add_source_files(ROOT / "source" / "counter" / "*.vhd")
+lib.add_source_files(ROOT / "source" / "counter" / "counter_simulation" / "*.vhd") 
+
+VU.main()
