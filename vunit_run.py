16728d1ed71bccb6ade62971fa286692a5a9d529
Janne Hannonen <janne.hannonen@eke.fi>
2023-05-25 08:40:00 +0300

    Remove 93 standard and unused component tests from vunit_run.py

--- vunit_run.py
+++ vunit_run.py
@@ -6,7 +6,7 @@ from vunit import VUnit
 # ROOT
 ROOT = Path(__file__).resolve().parent
 VU = VUnit.from_argv()
-VU = VUnit.from_argv(vhdl_standard="93")
+#VU = VUnit.from_argv(vhdl_standard="93")
 
 lib = VU.add_library("lib");
 lib.add_source_files(ROOT / "source/counter" / "*.vhd")
